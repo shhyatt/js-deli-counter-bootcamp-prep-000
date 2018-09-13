bf4ed1b8217431148d595862c837c864de1a5e96
Samantha Hyatt <shhyatt@gmail.com>
2018-09-13 20:01:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,13 +21,13 @@ function takeANumber(katzDeli, name) {
       function currentLine(katzDeli) {
         let i = 0;
         while(i < katzDeli.length) {
-          line.push(` ` +[i+1]+`.`+  katzDeli[i]);
+          line.push(` `  +[i+1]+`.`+  katzDeli[i]);
           i++;
           }
           if(katzDeli.length === 0) {
             return "The line is currently empty.";
           } else 
-          return (`The line is currently:`+ line);
+          return (`The line is currently:` + line);
            }
       
       
